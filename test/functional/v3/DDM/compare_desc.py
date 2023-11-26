2da78ca1d1fe89d6eea42700d62eb9a2dc71dd0d
nr24119 <35559324+nr24119@users.noreply.github.com>
2023-11-26 16:54:35 +0100

    code cleanup

--- test/functional/v3/DDM/compare_desc.py
+++ test/functional/v3/DDM/compare_desc.py
@@ -9,11 +9,11 @@ def test_comparing_descriptors():
     with Controller.from_port(port=5053) as controller:
         controller.authenticate()
 
-        # get hidden service descriptor of a regular onion service (used
+        # get hidden service descriptor of a regular onion service
         desc_regular = controller.get_hidden_service_descriptor('fzleuxt5okkdxn7ywujxllx3udod6pewolfofyghfrbpaj2a6vrpzeyd')
         onion_address_regular = 'fzleuxt5okkdxn7ywujxllx3udod6pewolfofyghfrbpaj2a6vrpzeyd.onion'
 
-        # needed for underlying functions to decrypt descriptor
+        # needed for underlying functions to decrypt the descriptor
         descriptor_text_regular = str(desc_regular).encode('utf-8')
 
         # call underlying functions used in Onionbalance to decrypt the outer and inner layer of the descriptor of a backend instance
